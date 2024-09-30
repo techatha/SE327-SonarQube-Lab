a8163130df07ad5dd164a59d19a6533a0895143a
techatha <techatham_p@cmu.ac.th>
2024-09-30 14:42:03 +0700

    init

---
+++ src/main/java/AdvanceCalculator.java
@@ -0,0 +1,12 @@
+public class AdvanceCalculator extends Calculator{
+    public double power(int base, int exponent){
+        return Math.pow(base, exponent);
+    }
+
+    public double sqrt(int a) throws IllegalArgumentException{
+        if (a<0){
+            throw new IllegalArgumentException("Cannot calculate square root of a negative number.");
+        }
+        return  Math.sqrt(a);
+    }
+}
